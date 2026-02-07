43b491c46a852f6e32f70cd4603f97449055133f
Nandam Vivekananda <76095061+vivek0402@users.noreply.github.com>
2026-02-07 11:30:10 +0530

    Fix typo in column name from 'material' to 'martial'

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -20,7 +20,7 @@ cst_id INT,
 cst_key NVARCHAR(50),
 cst_firstname NVARCHAR(50),
 cst_lastname NVARCHAR(50),
-cst_material_status NVARCHAR(50),
+cst_martial_status NVARCHAR(50),
 cst_gndr NVARCHAR(50),
 cst_create_date DATE
 );
